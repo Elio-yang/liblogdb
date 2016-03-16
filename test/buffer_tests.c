5b34b6a69bd4f482afe11ec3a18c39c4c13fb9c6
Jonas Schnelli <dev@jonasschnelli.ch>
2016-03-16 14:04:07 +0100

    Extend buffer.c test coverage

--- test/buffer_tests.c
+++ test/buffer_tests.c
@@ -15,9 +15,13 @@ void test_buffer()
     struct const_buffer buf0 = {"data", 4};
     struct const_buffer buf1 = {"data", 4};
     struct buffer* buf2;
-
+    struct buffer* buf3;
+    
+    uint64_t max = -1;
     assert(buffer_equal(&buf0.p, &buf1.p) == 1);
 
     buf2 = buffer_copy(&buf0.p, buf0.len);
     buffer_free(buf2);
+
+    buf3 = buffer_copy(&buf0.p, max);
 }
